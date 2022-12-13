37b016530facfc2b6369c74298642bf6470c3ec2
hieunt0303 <bibi030301@gmail.com>
2022-12-13 20:58:40 +0700

    init file

---
+++ index.ts
@@ -0,0 +1,2 @@
+console.log(123123);
+console.log("nguyetru nguyenngh");
---
+++ src/leatures/04-main.test.ts
@@ -0,0 +1,7 @@
+import { isPositive } from "./04-main";
+
+describe('isPositive()', () => {
+  it('should return true when n>0', () => {
+    expect(isPositive(1)).toBe(true);
+  });
+});
